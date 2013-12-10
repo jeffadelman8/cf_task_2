c6a4fb2cd31dc6764ed20592e84549ba19adec4c
Jeff Adelman <jeffadelman8@yahoo.com>
2013-12-09 23:52:36 -0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#button').click(function(){
+        var toAdd = $('input[name=checkListItem]').val();
+        $('.list').append('<div class="item"><span>' + 'X ' + '</span>' + toAdd + '</div>');
+    });
+    $(document).on('click', '.item', function(){
+        $(this).remove();
+    });
+});
